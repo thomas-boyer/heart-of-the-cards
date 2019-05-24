73f8ba58dd003081404241d82414b6c21a4abcfb
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-24 16:53:07 +0000

    Refactored command-line version of game

--- shell-game.js
+++ shell-game.js
@@ -219,23 +219,6 @@ const suits =
   3: 'diamonds'
 };
 
-const cards =
-[
-  {1: 'ace'},
-  {2: 2},
-  {3: 3},
-  {4: 4},
-  {5: 5},
-  {6: 6},
-  {7: 7},
-  {8: 8},
-  {9: 9},
-  {10: 10},
-  {11: 'jack'},
-  {12: 'queen'},
-  {13: 'king'}
-]
-
 const playerOneCards =
 [
   {1: 'ace'},
@@ -504,28 +487,3 @@ const main = async () => {
 }
 
 main();
-
-
-
-
-
-
-// let rank;
-// if (card.rank === 1)
-// {
-//   rank = 'ace';
-// }
-// else if (card.rank === 11)
-// {
-//   rank = 'jack';
-// }
-// else if (card.rank === 12)
-// {
-//   rank = 'queen';
-// }
-// else if (card.rank === 13)
-// {
-//   rank = 'king';
-// }
-// else rank = card.rank;
-
