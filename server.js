b34afbf59d541e1057ff3e5ddfe5cdf3df6be2cc
JSKwok <justin.s.kwok@gmail.com>
2019-05-27 05:22:57 -0400

    Minor changes

--- server.js
+++ server.js
@@ -201,12 +201,15 @@ io.on('connection', async (socket) => {
     if (connectCounter === 0) {
       gameID = await writeNewGame();
 
+      console.log("Handshake:", socket.handshake);
+
       let cookie = socket.handshake.headers.cookie;
       let userID = cookie.substring(cookie.indexOf("id=") + 3);
       if (userID.includes(';'))
       {
         userID = userID.substring(0, userID.indexOf(";"));
       }
+      console.log("cookie", cookie);
 
       p1id = socket.id;
 
@@ -494,6 +497,9 @@ const finalizeGame = async (data, gameID) =>
   const gamePlayerMax = await knex('game_players').max('id');
   const gamePlayerID = gamePlayerMax[0].max + 1;
 
+  console.log(data.playerOne);
+  console.log(data.playerTwo);
+
   await knex('games')
     .where('id', gameID)
     // Determine the prize suit and prize deck order in front-end, and send that through request body
@@ -501,6 +507,28 @@ const finalizeGame = async (data, gameID) =>
       date_played: new Date(),
       prize_suit: data.prize_suit,
       prize_deck: data.prize_deck });
+
+    knex('players')
+      .select('id').where('id', data.playerOne)
+      .then( (result) =>
+        {
+          if (!result.length)
+          {
+
+            return knex('players').insert({ id: data.playerOne, games_won: 0 });
+          }
+        });
+
+    knex('players')
+      .select('id').where('id', data.playerTwo)
+      .then( (result) =>
+        {
+          if (!result.length)
+          {
+            return knex('players').insert({ id: data.playerTwo, games_won: 0 });
+          }
+        });
+
     //Determine the player IDs and their suits in the front-end
   await knex('game_players')
     .insert([{
